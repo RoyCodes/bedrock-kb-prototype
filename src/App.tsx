50341841907f950b49fc9342276234d1911380ef
Roy Kincaid <roy.kincaid@mercedes-benz.com>
2024-12-07 12:24:17 +0100

    added Cloudscape Layout components and Tabs. Removed custom CSS for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@cloudscape-design/global-styles/index.css";
+import "@aws-amplify/ui-react/styles.css";
+import Tabs from "@cloudscape-design/components/tabs";
+import ContentLayout from "@cloudscape-design/components/content-layout";
+import Container from "@cloudscape-design/components/container";
+import Button from "@cloudscape-design/components/button";
+
+
 
 const client = generateClient<Schema>();
 
@@ -20,25 +27,66 @@ function App() {
   }
 
   return (
-    <Authenticator>
-      {({ signOut, user}) => (
-      <main>
-        <h1></h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>{todo.content}</li>
-          ))}
-        </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
-        </div>
-        <button onClick={signOut}> Sign out </button>
-      </main>
+    <Authenticator
+      components={{
+        SignIn: {
+          Header() {
+            return <h1 style={{ textAlign: "center" }}>Sign In</h1>;
+          },
+          Footer() {
+            return <div style={{ textAlign: "center" }}>Need help? Contact support.</div>;
+          },
+        },
+        SignUp: {
+          Header() {
+            return <h1 style={{ textAlign: "center" }}>Create a New Account</h1>;
+          },
+          Footer() {
+            return <div style={{ textAlign: "center" }}>Welcome to the platform!</div>;
+          },
+        },
+      }}
+    >
+      {({ signOut, user }) => (
+        <main>
+          <ContentLayout>
+            <Container header={<h1>Welcome, {user?.username || "User"}!</h1>}>
+              <Tabs
+                tabs={[
+                  {
+                    label: "Admin",
+                    id: "first",
+                    content: (
+                      <div>
+                        <button onClick={createTodo}>+ new</button>
+                        <ul>
+                          {todos.map((todo) => (
+                            <li key={todo.id}>{todo.content}</li>
+                          ))}
+                        </ul>
+                        <div>
+                          🥳 App successfully hosted. Try creating a new todo.
+                          <br />
+                          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                            Review next step of this tutorial.
+                          </a>
+                        </div>
+                      </div>
+                    ),
+                  },
+                  {
+                    label: "User",
+                    id: "second",
+                    content: "Second tab content area"
+                  }
+                ]}
+                variant="container"
+              />
+              <Button onClick={signOut}> Sign out </Button>
+            </Container>
+          </ContentLayout>
+
+        </main>
       )}
     </Authenticator>
   );
